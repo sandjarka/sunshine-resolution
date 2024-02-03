939054fbbcccb750ceae6dbef9b570783fd48ef0
sjrk <sjrk.dev@gmail.com>
2024-02-03 12:26:27 +0300

    main logic

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sunshine Resolution
+Small semi-automatic cli util to change resolution while streaming via sunshine.
+Primarily made to learn some rust :-)
+
+## Usage
+1. Download and unzip archive anywhere you want.
+2. Check config.toml file to change resolutions and/or path to sunshine.conf.
+3. Run sunshine-resolution.exe with admin privileges. It will inject callback to this app (needs to be done only once).
+   3.1. Re-run .exe file is location of sunshine or .exe file has changed.
+
+P.S. Even thought it is made for automatic usage you can still use as a cli program.
+```
+$ .\sunshine-resolution.exe --help
+Usage: sunshine-resolution.exe [OPTIONS]
+
+Options:
+  -d, --default  Change resolution to default value
+  -c, --custom   Change resolution to custom value
+  -h, --help     Print help
+  -V, --version  Print version
+```
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,122 @@
 use std::collections::HashMap;
 use std::env;
+use std::io::Write;
+use std::path::Path;
+use std::time::SystemTime;
+use clap::{arg, Parser};
 use config::Config;
 use displayz::{query_displays, refresh, Resolution};
+use log::{info, warn};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Change resolution to default value
+    #[arg(short, long)]
+    default: bool,
+
+    /// Change resolution to custom value
+    #[arg(short, long)]
+    custom: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    setup_logger().expect("Couldn't setup logger");
+    info!("Starting sunshine-resolution.");
+    let args = Args::parse();
+    let config = get_config();
+    if args.default {
+        info!("Setting display resolution to default.");
+        let default_resolution_str = config.get("default").expect("No default resolution is set");
+        change_resolution(default_resolution_str);
+    }
+    else if args.custom {
+        info!("Setting display resolution to custom.");
+        let custom_resolution_str = config.get("custom").expect("No custom resolution is set");
+        change_resolution(custom_resolution_str);
+    } else {
+        info!("Injecting sunshine-resolution callbacks to sunshine.conf");
+        let sunshine_conf_path = config.get("sunshine_conf").expect("No sunshine.conf path is set");
+        inject_to_sunshine_conf(sunshine_conf_path)
+    }
+    info!("Stopping sunshine-resolution.\n");
+}
+
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log").unwrap())
+        .apply().unwrap();
+    Ok(())
+}
+
+
+fn get_config() -> HashMap<String, String> {
     let config_file = Config::builder()
         .add_source(config::File::with_name("config"))
         .build()
         .expect("Couldn't find config.toml");
-
-    let config = config_file.try_deserialize::<HashMap<String, String>>()
-        .expect("Couldn't deserialize config content");
-
-    if &args.len() < &2 {
-        println!("Resolution argument is not passed");
-        return
-    }
-
-    let arg = &args[1];
-    let new_resolution = if arg == "custom" {
-        let custom_resolution_str = config.get("custom").expect("No custom resolution is set");
-        parse_to_resolution(custom_resolution_str.to_owned())
-    } else {
-        let default_resolution_str = config.get("default").expect("No default resolution is set");
-        parse_to_resolution(default_resolution_str.to_owned())
-    };
-    let current_resolution = get_current_resolution();
-    if new_resolution != current_resolution {
-        set_resolution(new_resolution)
-    } else {
-        println!("New resolution is the same!")
-    }
+    config_file.try_deserialize::<HashMap<String, String>>()
+        .expect("Couldn't deserialize config content")
 }
 
-fn parse_to_resolution(input: String) -> Resolution {
-    let parts: Vec<&str> = input.split('x').collect();
+fn inject_to_sunshine_conf(sunshine_conf_path: &String) {
+    let file_path = Path::new(sunshine_conf_path);
+    let mut file = std::fs::OpenOptions::new().write(true).truncate(true).open(&file_path)
+        .expect("Can't open starlight-stop.txt");
+    let current_dir = env::current_dir().unwrap();
+    let result= format!(r#"global_prep_cmd = [
+    {{
+        "do": "powershell.exe cd '{dir}'; ./sunshine-resolution --custom",
+        "undo": "powershell.exe cd '{dir}'; ./sunshine-resolution --default"
+    }}]"#, dir = current_dir.display().to_string().replace("\\", "\\\\"));
+    file.write(result.as_bytes()).expect("Can't write to starlight-stop.txt");
+    file.flush().unwrap();
+    info!("Injection is successfully completed!");
+}
 
+fn change_resolution(resolution: &String) {
+    let new_resolution = parse_to_resolution(resolution.to_owned())
+        .expect("Couldn't parse new resolution");
+    info!("Desired resolution is {resolution}.");
+    let current_resolution = get_current_resolution()
+        .expect("Couldn't get current resolution");
+    if new_resolution != current_resolution {
+        set_resolution(new_resolution)
+    } else { warn!("New resolution is the same!. Can't change anything.") }
+}
+
+fn parse_to_resolution(input: String) -> Option<Resolution> {
+    let parts: Vec<&str> = input.split('x').collect();
     if let [width, height] = parts.as_slice() {
         let width: u32 = width.parse().unwrap();
         let height: u32 = height.parse().unwrap();
-        Resolution::new(width, height)
-    } else {
-        println!("Invalid input format");
-        Resolution::new(0,0)
-    }
+        Some(Resolution::new(width, height))
+    } else { None }
 }
 
-pub fn get_current_resolution() -> Resolution {
+pub fn get_current_resolution() -> Option<Resolution> {
     let displays = query_displays().expect("No primary display found!");
-    return match displays.primary().settings() {
-        Some(settings) => {
-            (*settings).borrow().resolution
-        }
-        None => Resolution::new(0, 0)
-    }
+    if let Some(settings) = displays.primary().settings() {
+        Some((*settings).borrow().resolution)
+    } else { None }
 }
 
 pub fn set_resolution(new_resolution: Resolution) {
     let displays = query_displays().expect("No primary display found!");
-    match displays.primary().settings() {
-        Some(settings) => {
-            (*settings).borrow_mut().resolution = new_resolution;
-        }
-        None => {
-            println!("Primary display has no settings!");
-        }
+    if let Some(settings) = displays.primary().settings() {
+        (*settings).borrow_mut().resolution = new_resolution;
+        displays.primary().apply().expect("Can't apply new settings to display");
+        refresh().expect("Can't apply new settings to display");
     }
-    displays.primary().apply().expect("Can't apply new settings to display");
-    refresh().expect("Can't apply new settings to display");
 }
